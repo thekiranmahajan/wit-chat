a042bb2eb1c57cfa63d1b0061e16f975f7281cc9
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-03-10 17:51:32 +0530

    feat: created userModel containing user credentials as Schema for database

---
+++ backend/models/userModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const userModel = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    avatar: {
+      type: String,
+      default:
+        "https://i.pinimg.com/564x/0d/64/98/0d64989794b1a4c9d89bff571d3d5842.jpg",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userModel);
+export default User;
