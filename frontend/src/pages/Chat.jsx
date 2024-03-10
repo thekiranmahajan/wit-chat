00192f1f5b767cc3a312b83b97091309d31a2948
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-03-10 13:16:32 +0530

    feat: render data coming from backend's api into frontend using axios

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Chat = () => {
-  return <div>Chat Page</div>;
+  const [chats, setChats] = useState([]);
+  const fetchChats = async () => {
+    try {
+      const { data } = await axios.get("/api/chat");
+      setChats(data);
+      console.log(data);
+    } catch (error) {
+      console.error("There was a problem fetching the chats:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchChats();
+  }, []);
+
+  return (
+    <div className="text-2xl flex items-center justify-center flex-col">
+      Chat Page: {chats?.length}
+      {chats?.map((chat) => (
+        <div key={chat?._id}>{chat?._id}</div>
+      ))}
+    </div>
+  );
 };
 
 export default Chat;
