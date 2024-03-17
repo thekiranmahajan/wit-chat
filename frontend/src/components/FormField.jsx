8895921244edea103257d1315922a4bd6529bea2
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-03-17 18:42:40 +0530

    feat: utilise reausability with multi-purpose input component

--- frontend/src/components/FormField.jsx
+++ frontend/src/components/FormField.jsx
@@ -1,14 +1,31 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const FormField = ({ inputType, label, icon, id }) => {
+const FormField = ({
+  inputType,
+  label,
+  iconName,
+  id,
+  placeholder,
+  isRequired,
+}) => {
   return (
-    <>
-      <label htmlFor={id}>{label}</label>
-      <div>
-        <img src={icon} alt={icon} />
-        <input type={inputType} id={id} />
+    <div className="px-4 mt-2 flex flex-col gap-1 ">
+      <label className="font-semibold" htmlFor={id}>
+        {label} {isRequired && <sup className="text-red-500 text-base">*</sup>}
+      </label>
+
+      <div className="h-10 rounded-md flex items-center px-4 bg-[#312C4F]">
+        <FontAwesomeIcon icon={iconName} />
+        <input
+          className="bg-transparent outline-none border-none w-full p-4 truncate placeholder:text-sm"
+          type={inputType}
+          placeholder={placeholder}
+          id={id}
+          required={isRequired}
+        />
       </div>
-    </>
+    </div>
   );
 };
 
