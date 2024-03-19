174d30973604bcfeb052440e18c10f28677f1386
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-03-19 20:25:53 +0530

    feat: remove unnecessary routes and adding a middleware with userRoutes

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-import chats from "./data/dummyChats.js";
 import connectDB from "./config/db.js";
 import colors from "colors";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 dotenv.config();
 connectDB();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API initiated!");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server is running on PORT ${PORT}`.green.bold));
