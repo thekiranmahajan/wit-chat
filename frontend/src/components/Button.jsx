d9333886d6ad6df326a800523e828406b00061d6
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-03-20 16:35:46 +0530

    feat: create reusable button component for modular code

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { loader } from "../assets";
+
+const Button = ({ type, isLoading, onClick, title, styles }) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`h-10  w-3/5 rounded-lg font-extrabold text-lg cursor-pointer flex items-center justify-center ${styles}`}
+    >
+      {isLoading ? <img className="h-6" src={loader} alt="loader" /> : title}
+    </button>
+  );
+};
+
+export default Button;
