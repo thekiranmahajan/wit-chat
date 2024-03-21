bbf725abcbfeb002f92325be1e8b872b8363b28a
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-03-21 11:11:43 +0530

    feat: changes for deployment over the render.com

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,13 @@ import connectDB from "./config/db.js";
 import colors from "colors";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-
+import path from "path";
 const app = express();
 dotenv.config();
 connectDB();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API initiated!");
-});
 app.get("/api/env", (req, res) => {
   const safeEnv = {
     CLOUD_NAME: process.env.REACT_APP_CLOUD_NAME,
@@ -22,6 +19,22 @@ app.get("/api/env", (req, res) => {
   res.json(safeEnv);
 });
 app.use("/api/user", userRoutes);
+
+// ----------------Deployment-----------------
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API initiated!");
+  });
+}
+// ----------------Deployment-----------------
+
 app.use(notFound);
 app.use(errorHandler);
 
