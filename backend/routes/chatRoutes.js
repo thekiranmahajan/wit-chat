52b017bf86d83390e1609473a3cffaa7be39b5f2
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-03-21 20:18:31 +0530

    feat: create accessChat controller to access chat or to initiate chat with given userId + fix import issues

---
+++ backend/routes/chatRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import authenticateRequest from "../middleware/authMiddleware.js";
+import { accessChat } from "../controllers/chatControllers.js";
+
+const router = express.Router();
+
+router.route("/").post(authenticateRequest, accessChat);
+
+// router.route("/").get(authenticateRequest, fetchChats);
+
+// router.route("/create-group").post(authenticateRequest, createGroup);
+
+// router.route("/rename-group").put(authenticateRequest, renameGroup);
+
+// router.route("/remove-member").post(authenticateRequest, removeFromGroup);
+
+// router.route("/add-member").post(authenticateRequest, addToGroup);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from "colors";
 import userRoutes from "./routes/userRoutes.js";
+import chatRoutes from "./routes/chatRoutes.js";
 import {
   notFound,
   errorHandler,
@@ -22,6 +23,7 @@ app.get("/api/env", (req, res) => {
   res.json(safeEnv);
 });
 app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 // ----------------Deployment-----------------
 const __dirname1 = path.resolve();
