b25d7d576819c0350154721555e6ec2db8748d05
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-03-26 22:06:56 +0530

    feat: create MyChat component rendering all user chats

--- frontend/src/components/MyChats.jsx
+++ frontend/src/components/MyChats.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { ChatState } from "../context/ChatProvider";
 import axios from "axios";
-
+import Button from "./Button";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import UserSearchShimmer from "./UserSearchShimmer";
+import { getSender } from "../constants/chatDataRetrieval";
 const MyChats = () => {
   const [loggedUser, setLoggedUser] = useState();
 
@@ -21,14 +24,59 @@ const MyChats = () => {
       });
     }
   };
-
+  const HandleGroupChat = () => {};
   useEffect(() => {
     setLoggedUser(JSON.parse(localStorage.getItem("userInfo")));
     fetchChats();
   }, []);
+
   return (
-    <div className="blurEffect w-2/6 h-full rounded-lg flex flex-col p-5 shadow-lg">
-      MyChats
+    <div className="blurEffect w-2/6 h-full rounded-lg flex flex-col p-5 shadow-lg items-center gap-4">
+      <div className="flex w-full items-center justify-between flex-col lg:flex-row gap-2">
+        <h2 className="text-2xl  font-extrabold">My Chats</h2>
+        <Button
+          title="Create A Group Chat"
+          styles="bg-[#002133] shadow-lg  relative hover:scale-105 transition-transform active:scale-95 duration-300 font-bold truncate w-full   lg:w-[80%] lg:max-w-64"
+          iconName={faPlus}
+          onClick={HandleGroupChat}
+        />
+      </div>
+
+      <div className="h-full w-11/12 overflow-y-scroll no-scrollbar overflow-x-hidden py-5 px-2 ">
+        {chats.length !== 0 ? (
+          <div className="flex flex-col">
+            {chats.map((chat) => (
+              <div
+                key={chat._id}
+                onClick={() => setSelectedChat(chat)}
+                className={`bg-[#006761] mt-2 h-14 w-full rounded-md flex items-center p-3 overflow-hidden cursor-pointer hover:scale-105 active:scale-95 transition-all duration-300 ${
+                  selectedChat === chat && "bg-[#91AA66]"
+                }`}
+              >
+                <div className="sm:h-10 sm:w-10 h-8 w-8 rounded-full overflow-hidden flex items-center justify-center">
+                  <img
+                    className="h-full w-full object-cover rounded-full"
+                    src={
+                      getSender(chat?.users, loggedUser)?.avatar ||
+                      "https://i.pinimg.com/564x/98/53/c5/9853c5ae293810fc37fb567c8940c303.jpg"
+                    }
+                    alt="avatar"
+                  />
+                </div>
+                <div className="relative leading-5 truncate h-12 w-3/4 flex flex-col justify-center py-1 ml-4">
+                  <h2 className=" ">
+                    {chat?.isGroupChat
+                      ? chat?.chatName
+                      : getSender(chat?.users, loggedUser)?.name}
+                  </h2>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <UserSearchShimmer noOfTimes={9} />
+        )}
+      </div>
     </div>
   );
 };
---
+++ frontend/src/constants/chatDataRetrieval.js
@@ -0,0 +1,3 @@
+export const getSender = (users, loggedUser) => {
+  return users[0]?._id === loggedUser?._id ? users[1] : users[0];
+};
