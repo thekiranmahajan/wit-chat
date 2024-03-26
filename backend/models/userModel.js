939b45da8830cd12261a76b3ed4377f735da4f0b
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-03-26 15:29:50 +0530

    feat: change default avatar url

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -8,7 +8,7 @@ const userSchema = mongoose.Schema(
     avatar: {
       type: String,
       default:
-        "https://i.pinimg.com/564x/0d/64/98/0d64989794b1a4c9d89bff571d3d5842.jpg",
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
   },
   { timestamps: true }
