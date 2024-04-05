3c2b4c2bd5ed4e700b313a0dfbceaf6069c9ea8b
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-04-05 20:55:49 +0530

    feat: create 2 functions to determine the message is lastMessage or from sameSender from the messages array

--- frontend/src/constants/chatDataRetrieval.js
+++ frontend/src/constants/chatDataRetrieval.js
@@ -1,3 +1,20 @@
 export const getSender = (users, loggedUser) => {
   return users[0]?._id === loggedUser?._id ? users[1] : users[0];
 };
+
+export const isSameSender = (messages, message, index, userId) => {
+  return (
+    index < messages?.length - 1 &&
+    (messages[index + 1]?.sender?._id !== message?.sender?._id ||
+      messages[index + 1]?.sender?._id === undefined) &&
+    messages[index]?.sender?._id !== userId
+  );
+};
+
+export const isLastMessage = (messages, index, userId) => {
+  return (
+    index === messages?.length - 1 &&
+    messages[messages?.length - 1]?.sender?._id !== userId &&
+    messages[messages?.length - 1]?.sender?._id
+  );
+};
